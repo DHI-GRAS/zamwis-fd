b761b9bc9f810596fcaae83d7af0e607cc21b7b1
Jonas <josl@dhi-gras.com>
2016-11-11 14:44:36 +0100

    adjust date defaults

--- zamwis_fd/run.py
+++ zamwis_fd/run.py
@@ -15,19 +15,20 @@ logging.basicConfig(format='%(module)s - %(message)s', level=logging.INFO)
 @click.command()
 @click.argument('outdir', type=click.Path(exists=True))
 @click.option('--extent', default='18.3,36.6,-20.5,-8.9', help='Extent of interest (xmin,xmax,ymin,ymax) (default: \'18.3,36.6,-20.5,-8.9\')')
-@click.option('--startdate', help='Start date YYYYMMDD')
-@click.option('--enddate', help='End date YYYYMMDD')
+@click.option('--startdate', help='Download query start date YYYYMMDD (default: a year ago)')
+@click.option('--enddate', help='Download query end date YYYYMMDD (default: today)')
 @click.option('--split/--nosplit', default=True, help='Split output files into yearly chunks (default: True)')
 def main(outdir, extent,
         startdate=None, enddate=None,
         split=True):
+    """Download and process NDVI, SWI10, and TRMM for ZAMWIS database"""
 
     if enddate is None:
         enddate = datetime.datetime.now()
         enddate_str = enddate.strftime('%Y%m%d')
 
     if startdate is None:
-        startdate = enddate - datetime.timedelta(days=10)
+        startdate = enddate - datetime.timedelta(days=365)
         startdate_str = startdate.strftime('%Y%m%d')
 
     workflow.update_products(outdir=outdir, extent=extent,
