f744efc1e5d1779638d7080d7f529aab436530a8
Jonas <josl@dhi-gras.com>
2016-11-14 15:29:39 +0100

    fix interface

--- zamwis_fd/run.py
+++ zamwis_fd/run.py
@@ -24,11 +24,10 @@ def main(outdir, extent,
     """Download and process NDVI, SWI10, and TRMM for ZAMWIS database"""
 
     if enddate is None:
-        enddate = datetime.datetime.now()
-        enddate = enddate.strftime('%Y%m%d')
+        enddate = datetime.datetime.now().strftime('%Y%m%d')
 
     if startdate is None:
-        startdate = enddate - datetime.timedelta(days=365)
+        startdate = datetime.datetime.strptime(enddate, '%Y%m%d') - datetime.timedelta(days=365)
         startdate = startdate.strftime('%Y%m%d')
 
     workflow.update_products(outdir=outdir, extent=extent,
