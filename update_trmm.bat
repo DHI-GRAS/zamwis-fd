9252aa1bd3e19c473905241686f6fa764b59cca4
Jonas Sølvsteen <j08lue@gmail.com>
2018-01-17 13:21:00 +0100

    split up into one bat file per product

---
+++ update_trmm.bat
@@ -0,0 +1,25 @@
+SETLOCAL ENABLEEXTENSIONS
+SET PARENT=%~dp0
+
+set TRMM_EXTENT="18.375,36.5,-20.25,-8.875"
+
+REM set DOWNLOAD_START="--startdate=20160101"
+REM set DOWNLOAD_END="--enddate=20160201"
+
+set "TRMM_DIR=%PARENT%\TRMM"
+set "TRMM_FILE=%TRMM_DIR%\TRMM.nc"
+
+set "EXPORT_DIR=%PARENT%\export"
+set "EXPORT_DIR_TRMM=%EXPORT_DIR%\TRMM"
+
+call activate fd
+
+call flooddrought download_trmm --extent %TRMM_EXTENT% %TRMM_FILE% %DOWNLOAD_START% %DOWNLOAD_END% --no-delete
+call flooddrought save_spi_stats %TRMM_FILE%
+call flooddrought calculate_rainfall_indices %TRMM_FILE%
+call flooddrought save_timeslice_geotiff %TRMM_FILE% %EXPORT_DIR_TRMM%
+call flooddrought save_timeslice_geotiff %TRMM_DIR%\indices\*_1_month*.nc %EXPORT_DIR_TRMM%\1_month
+call flooddrought save_timeslice_geotiff %TRMM_DIR%\indices\*_3_month*.nc %EXPORT_DIR_TRMM%\3_month
+call flooddrought save_timeslice_geotiff %TRMM_DIR%\indices\*_6_month*.nc %EXPORT_DIR_TRMM%\6_month
+
+pause
