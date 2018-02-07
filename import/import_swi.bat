c2ca5dcaf6d1cffba2064ca3a66e709efa40d777
Jonas Sølvsteen <j08lue@gmail.com>
2018-02-07 16:05:24 +0100

    no anomaly #6

--- import/import_swi.bat
+++ import/import_swi.bat
@@ -2,5 +2,5 @@ setlocal enableextensions
 
 if not defined EXPORT_DIR_SWI exit /b -1 
 
-call import\runscript.bat "/Import/ImportAndAppendTemporalRaster" importDir=%EXPORT_DIR_SWI%\anomaly "rasterName=swi" "datetimeFileMask=yyyyMMdd" "append=TRUE"
+call import\runscript.bat "/Import/ImportAndAppendTemporalRaster" importDir=%EXPORT_DIR_SWI% "rasterName=swi" "datetimeFileMask=yyyyMMdd" "append=TRUE"
 call import\runscript.bat "/Import/ImportAndAppendTemporalRaster" importDir=%EXPORT_DIR_SWI%\deviation "rasterName=swi_dev" "datetimeFileMask=yyyyMMdd" "append=TRUE"
